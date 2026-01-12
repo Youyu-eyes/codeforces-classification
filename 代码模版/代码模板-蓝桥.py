2ef59382015fce306cda3deab85858414c592069
Youyu-eyes <2630921077@qq.com>
2026-01-12 22:28:29 +0800

    蓝桥的模板

---
+++ 代码模版/代码模板-蓝桥.py
@@ -0,0 +1,29 @@
+# 以下的库函数蓝桥可用，pairwise，cache 蓝桥不让用
+
+from math import inf
+from math import gcd
+from math import sin
+from math import pi
+from heapq import heapify
+from heapq import heappush
+from heapq import heappop
+from heapq import heapreplace
+from heapq import nsmallest
+from heapq import nlargest
+from itertools import accumulate
+from itertools import zip_longest
+from bisect import bisect_left
+from bisect import bisect_right
+from collections import defaultdict
+from collections import Counter
+from collections import deque
+import datetime
+import re
+from typing import List, Tuple
+si = lambda: input()
+ii = lambda: int(input())
+many_ii = lambda: map(int, input().split())
+many_si = lambda: input().split()
+lii = lambda: list(many_ii())
+lsi = lambda: list(many_si())
+MOD = 1_000_000_007
