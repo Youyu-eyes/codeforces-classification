a2a522d40587d711eb6617e2c844207c99caee84
Youyu-eyes <2630921077@qq.com>
2026-01-16 21:09:09 +0800

    记忆化搜索

--- 动态规划/区间DP/2064C. Remove the Ends.py
+++ 动态规划/区间DP/2064C. Remove the Ends.py
@@ -6,6 +6,8 @@
 # 写递推的时候要注意一点，l 要倒序枚举
 # 能过的方法见贪心题单
 
+from functools import cache
+
 import sys
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 print = lambda *args, end='\n', sep=' ': sys.stdout.write(
@@ -25,6 +27,20 @@ for _ in range(II()):
     n = II()
     a = LII()
 
+    # 记忆化搜索
+    @cache
+    def dfs(l, r):
+        if l == r:
+            return 0
+        res = 0
+        if a[i] > 0:
+            res = max(res, dfs(i + 1, r) + a[i])
+        else:
+            res = max(res, dfs(l, i) - a[i])
+        return res
+    print(dfs(0, n))
+
+    # 递推
     f = [[0] * (n + 1) for _ in range(n + 1)]
     for l in range(n - 1, -1, -1):
         for r in range(l + 1, n + 1):
