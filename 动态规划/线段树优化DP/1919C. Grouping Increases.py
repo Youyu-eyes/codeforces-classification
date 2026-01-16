d94ff21a9845a4413bb39bd8be3ca387401920da
Youyu-eyes <2630921077@qq.com>
2026-01-16 21:22:13 +0800

    lazy线段树优化DP

--- 动态规划/线段树优化DP/1919C. Grouping Increases.py
+++ 动态规划/线段树优化DP/1919C. Grouping Increases.py
@@ -2,55 +2,34 @@
 # Lazy 线段树优化DP
 # 1800 另有贪心做法
 
-from math import inf
-from math import gcd
-from math import lcm
-from math import sin
-from math import pi
-from heapq import heapify
-from heapq import heappush
-from heapq import heappop
-from heapq import heapreplace
-from heapq import nsmallest
-from heapq import nlargest
-from functools import cache
-from itertools import accumulate
-from itertools import zip_longest
-from itertools import pairwise
-from bisect import bisect_left
-from bisect import bisect_right
-from collections import defaultdict
-from collections import Counter
-from collections import deque
-import datetime
-import re
-from copy import deepcopy
-from typing import List, Tuple
-si = lambda: input()
-ii = lambda: int(input())
-many_ii = lambda: map(int, input().split())
-many_si = lambda: input().split()
-lii = lambda: list(many_ii())
-lsi = lambda: list(many_si())
-MOD = 1_000_000_007
-
-
-# Lazy 线段树优化DP
-# 这版过不了，应该是python的运行效率和这个Lazy线段树的板子问题，精简板子后 c++ 能过
+# 这版过不了，是python的运行效率，c++ 能过，请看下面一版
 # 能过的为贪心算法，详见贪心题单
 
+from math import inf
+from typing import List
+import sys
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+print = lambda *args, end='\n', sep=' ': sys.stdout.write(
+    sep.join(map(str, args)) + end
+)
+
+def II():
+    return int(input())
+
+def MII(b=0):
+    return map(lambda x: int(x) - b, input().split())
+
+def LII():
+    return list(MII())
 
 class Node:
     __slots__ = 'val', 'todo'
 
 class LazySegmentTree:
-    # 懒标记初始值
-    _TODO_INIT = 0  # **根据题目修改**
+    _TODO_INIT = 0
 
     def __init__(self, arr, default=0):
-        # 线段树维护一个长为 n 的数组（下标从 0 到 n-1）
-        # arr 可以是 list 或者 int
-        # 如果 arr 是 int，视作数组大小，默认值为 default
         if isinstance(arr, int):
             arr = [default] * arr
         n = len(arr)
@@ -58,81 +37,67 @@ class LazySegmentTree:
         self._tree = [Node() for _ in range(2 << (n - 1).bit_length())]
         self._build(arr, 1, 0, n - 1)
 
-    # 合并两个 val
     def _merge_val(self, a: int, b: int) -> int:
-        return min(a, b)  # **根据题目修改**
+        return min(a, b)
 
-    # 合并两个懒标记
     def _merge_todo(self, a: int, b: int) -> int:
-        return a + b  # **根据题目修改**
+        return a + b
 
-    # 把懒标记作用到 node 子树（本例为区间加）
     def _apply(self, node: int, l: int, r: int, todo: int) -> None:
         cur = self._tree[node]
-        # 修改这里：维护最小值，所以只需加 todo，不需要乘以区间长度
         cur.val += todo
         cur.todo = self._merge_todo(todo, cur.todo)
 
-    # 把当前节点的懒标记下传给左右儿子
     def _spread(self, node: int, l: int, r: int) -> None:
         todo = self._tree[node].todo
-        if todo == self._TODO_INIT:  # 没有需要下传的信息
+        if todo == self._TODO_INIT:
             return
         m = (l + r) // 2
         self._apply(node * 2, l, m, todo)
         self._apply(node * 2 + 1, m + 1, r, todo)
-        self._tree[node].todo = self._TODO_INIT  # 下传完毕
+        self._tree[node].todo = self._TODO_INIT
 
-    # 合并左右儿子的 val 到当前节点的 val
     def _maintain(self, node: int) -> None:
         self._tree[node].val = self._merge_val(self._tree[node * 2].val, self._tree[node * 2 + 1].val)
 
-    # 用 a 初始化线段树
-    # 时间复杂度 O(n)
     def _build(self, a: List[int], node: int, l: int, r: int) -> None:
         self._tree[node].todo = self._TODO_INIT
-        if l == r:  # 叶子
-            self._tree[node].val = a[l]  # 初始化叶节点的值
+        if l == r:
+            self._tree[node].val = a[l]
             return
         m = (l + r) // 2
-        self._build(a, node * 2, l, m)  # 初始化左子树
-        self._build(a, node * 2 + 1, m + 1, r)  # 初始化右子树
+        self._build(a, node * 2, l, m)
+        self._build(a, node * 2 + 1, m + 1, r)
         self._maintain(node)
 
     def _update(self, node: int, l: int, r: int, ql: int, qr: int, f: int) -> None:
-        if ql <= l and r <= qr:  # 当前子树完全在 [ql, qr] 内
+        if ql <= l and r <= qr:
             self._apply(node, l, r, f)
             return
         self._spread(node, l, r)
         m = (l + r) // 2
-        if ql <= m:  # 更新左子树
+        if ql <= m:
             self._update(node * 2, l, m, ql, qr, f)
-        if qr > m:  # 更新右子树
+        if qr > m:
             self._update(node * 2 + 1, m + 1, r, ql, qr, f)
         self._maintain(node)
 
     def _query(self, node: int, l: int, r: int, ql: int, qr: int) -> int:
-        if ql <= l and r <= qr:  # 当前子树完全在 [ql, qr] 内
+        if ql <= l and r <= qr:
             return self._tree[node].val
         self._spread(node, l, r)
         m = (l + r) // 2
-        if qr <= m:  # [ql, qr] 在左子树
+        if qr <= m:
             return self._query(node * 2, l, m, ql, qr)
-        if ql > m:  # [ql, qr] 在右子树
+        if ql > m:
             return self._query(node * 2 + 1, m + 1, r, ql, qr)
         l_res = self._query(node * 2, l, m, ql, qr)
         r_res = self._query(node * 2 + 1, m + 1, r, ql, qr)
         return self._merge_val(l_res, r_res)
 
-    # 用 f 更新 [ql, qr] 中的每个 a[i]
-    # 0 <= ql <= qr <= n-1
-    # 时间复杂度 O(log n)
     def update(self, ql: int, qr: int, f: int) -> None:
         self._update(1, 0, self._n - 1, ql, qr, f)
 
-    # 返回用 _merge_val 合并所有 a[i] 的计算结果，其中 i 在闭区间 [ql, qr] 中
-    # 0 <= ql <= qr <= n-1
-    # 时间复杂度 O(log n)
     def query(self, ql: int, qr: int) -> int:
         return self._query(1, 0, self._n - 1, ql, qr)
     
@@ -149,9 +114,13 @@ def discretize(arr):
     discretized = [value_to_index[x] for x in arr]
     return discretized, len(sorted_unique)
 
-def solve(n, nums):
+def solve():
+    n = II()
+    nums = LII()
+    
     if n <= 1:
-        return 0
+        print(0)
+        return
     
     nums, m = discretize(nums)
     
@@ -171,10 +140,8 @@ def solve(n, nums):
         if ndp < current_val:
             seg.set_val(nums[i - 1], ndp)
     
-    return seg.query(0, m)
+    print(seg.query(0, m))
 
-T = ii()
-for _ in range(T):
-    n = ii()
-    nums = lii()
-    print(solve(n, nums))
+if __name__ == "__main__":
+    for _ in range(II()):
+        solve()
