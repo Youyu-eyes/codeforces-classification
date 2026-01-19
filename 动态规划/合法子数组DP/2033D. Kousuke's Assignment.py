9ae13b6c9bc4d0619600bcbca68e58175a879529
Youyu-eyes <2630921077@qq.com>
2026-01-19 13:20:47 +0800

    子数组DP

--- 动态规划/合法子数组DP/2033D. Kousuke's Assignment.py
+++ 动态规划/合法子数组DP/2033D. Kousuke's Assignment.py
@@ -16,53 +16,54 @@ lsi = lambda: input()
 
 # 优化前
 import sys
-input = lambda: sys.stdin.readline().split()
-si = lambda: input()[0]
-ii = lambda: int(si())
-lii = lambda: list(map(int, input()))
-lsi = lambda: input()
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+print = lambda *args, end='\n', sep=' ': sys.stdout.write(
+    sep.join(map(str, args)) + end
+)
 
-def solve():
-    T = ii()
-    out = []
-    for _ in range(T):
-        n = ii()
-        nums = lii()
-        s = 0
-        cnt = {0: -1}
-        f = [0] * (n + 1)
-        for i, x in enumerate(nums):
-            s += x
-            f[i + 1] = f[i]
-            if s in cnt:
-                f[i + 1] = max(f[i + 1], f[cnt[s] + 1] + 1)
-            cnt[s] = i
+def II():
+    return int(input())
 
-        out.append(str(f[-1]))
-    sys.stdout.buffer.write('\n'.join(out).encode())
+def MII(b = 0):
+    return map(lambda x: int(x) - b, input().split())
 
-if __name__ == "__main__":
-    solve()
+def LII():
+    return list(MII())
+
+for _ in range(II()):
+    n = II()
+    nums = LII()
+    s = 0
+    cnt = {0: -1}
+    f = [0] * (n + 1)
+    for i, x in enumerate(nums):
+        s += x
+        f[i + 1] = f[i]
+        if s in cnt:
+            f[i + 1] = max(f[i + 1], f[cnt[s] + 1] + 1)
+        cnt[s] = i
+    print(f[-1])
 
 # 滚动数组优化，哈希表直接统计 max(f[cnt[s] + 1])
 # 这实际上已经是一个贪心了，见贪心题单
-def solve():
-    T = ii()
-    out = []
-    for _ in range(T):
-        n = ii()
-        nums = lii()
-        s = 0
-        cnt = {0: 0}
-        f = 0
-        for x in nums:
-            s += x
-            if s in cnt:
-                f = max(f, cnt[s] + 1)
-            cnt[s] = f
+def II():
+    return int(input())
 
-        out.append(str(f))
-    sys.stdout.buffer.write('\n'.join(out).encode())
+def MII(b = 0):
+    return map(lambda x: int(x) - b, input().split())
 
-if __name__ == "__main__":
-    solve()
+def LII():
+    return list(MII())
+
+for _ in range(II()):
+    n = II()
+    nums = LII()
+    s = 0
+    cnt = {0: 0}
+    f = 0
+    for x in nums:
+        s += x
+        if s in cnt:
+            f = max(f, cnt[s] + 1)
+        cnt[s] = f
+    print(f)
