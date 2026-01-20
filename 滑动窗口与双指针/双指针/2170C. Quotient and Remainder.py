f2d917f836b3f8c11df15ac6997bac9abf533b77
Youyu-eyes <2630921077@qq.com>
2026-01-20 14:00:39 +0800

    更新一下，好题

--- 滑动窗口与双指针/双指针/2170C. Quotient and Remainder.py
+++ 滑动窗口与双指针/双指针/2170C. Quotient and Remainder.py
@@ -1,60 +1,45 @@
 # https://codeforces.com/problemset/problem/2170/C
 # 好题
 # 1300
-# 贪心 + 式子变形：x = q(r + 1) + r <= k ==> qr + q + r <= k ==> (q + 1)(r + 1) <= (k + 1) ==> (r + 1) <= (k + 1) // (q + 1)
-# 排序 + 双指针 + 二分: 在 r 数组中二分，如果 r[p2] 不满足题意 q[p1]，q[p1] 变大，r[p2] 更不满足条件，可以直接缩小二分范围
+# 贪心 + 式子变形：
+# x = q(r + 1) + r <= k
+# ==> qr + q + r <= k
+# ==> (q + 1)(r + 1) <= (k + 1) 
+# ==> (r + 1) <= (k + 1) // (q + 1)
 
-from math import inf
-from math import gcd
-from math import lcm
-from math import sin
-from math import pi
-from heapq import heapify
-from heapq import heappush
-from heapq import heappop
-from heapq import heapreplace
-from heapq import nsmallest
-from heapq import nlargest
-from functools import cache
-from itertools import accumulate
-from itertools import zip_longest
-from itertools import pairwise
+# 排序 + 双指针 + 二分: 
+# 在 r 数组中二分，如果 r[p2] 不满足题意 q[p1]，q[p1] 变大，r[p2] 更不满足条件，可以直接缩小二分范围
+
 from bisect import bisect_left
-from bisect import bisect_right
-from collections import defaultdict
-from collections import Counter
-from collections import deque
-import datetime
-import re
-from copy import deepcopy
-from typing import List, Tuple
-si = lambda: input()
-ii = lambda: int(input())
-many_ii = lambda: map(int, input().split())
-many_si = lambda: input().split()
-lii = lambda: list(many_ii())
-lsi = lambda: list(many_si())
-MOD = 1_000_000_007
 
-def solve(n, k, lq, lr):
+import sys
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+print = lambda *args, end='\n', sep=' ': sys.stdout.write(
+    sep.join(map(str, args)) + end
+)
+
+def II():
+    return int(input())
+
+def MII(b = 0):
+    return map(lambda x: int(x) - b, input().split())
+
+def LII(b = 0):
+    return list(MII(b))
+
+for _ in range(II()):
+    n, k = MII()
+    lq = sorted(LII())
+    lr = sorted(LII(b = -1))
     p1 = 0
-    p2 = n  # p2 用开区间的好处是，由于 bisect_left 函数的右端点是开区间，开区间可以防止 p2 反复横跳
+
+    # p2 开区间，因为 bisect_left 右端点参数默认开区间
+    p2 = n
     ans = 0
-    lq.sort()
-    lr.sort()
     while p1 < n and p2 >= 0:
         q = lq[p1]
         p2 = bisect_left(lr, (k + 1) // (q + 1) + 1, 0, p2) - 1
         if p2 >= 0:
             ans += 1
         p1 += 1
-    return ans
-
-T = ii()
-for _ in range(T):
-    n, k = many_ii()
-    q = lii()
-    r = lii()
-    for i in range(n):
-        r[i] += 1
-    print(solve(n, k, q, r))
+    print(ans)
