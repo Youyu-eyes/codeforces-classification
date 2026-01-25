7d908c7e55fe8445398b4e7fe4a6924c09e5a039
Youyu-eyes <2630921077@qq.com>
2026-01-25 19:17:03 +0800

    不带权 LCA

--- 代码模版/LCA 最近公共祖先.py
+++ 代码模版/LCA 最近公共祖先.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 
+# 带权 LCA 模板
 class LcaBinaryLifting:
     def __init__(self, edges: List[List[int]]):
         n = len(edges) + 1
@@ -61,6 +62,66 @@ class LcaBinaryLifting:
     # 返回 x 到 y 的距离（最短路长度）
     def get_dis(self, x: int, y: int) -> int:
         return self.dis[x] + self.dis[y] - self.dis[self.get_lca(x, y)] * 2
+
+# 不带权 LCA 模板
+class LcaBinaryLifting:
+    def __init__(self, edges: List[List[int]]):
+        n = len(edges) + 1
+        m = n.bit_length()
+        g = [[] for _ in range(n)]
+        for x, y in edges:
+            # 如果题目的节点编号从 1 开始，改成 x-1 和 y-1
+            g[x].append(y)
+            g[y].append(x)
+
+        depth = [0] * n
+        pa = [[-1] * n for _ in range(m)]
+
+        def dfs(x: int, fa: int) -> None:
+            pa[0][x] = fa
+            for y in g[x]:
+                if y != fa:
+                    depth[y] = depth[x] + 1
+                    dfs(y, x)
+        dfs(0, -1)
+
+        for i in range(m - 1):
+            for x in range(n):
+                if (p := pa[i][x]) != -1:
+                    pa[i + 1][x] = pa[i][p]
+
+        self.depth = depth
+        self.pa = pa
+
+    # 返回 node 的第 k 个祖先节点
+    # 如果不存在，返回 -1
+    def get_kth_ancestor(self, node: int, k: int) -> int:
+        pa = self.pa
+        for i in range(k.bit_length()):
+            if k >> i & 1:
+                node = pa[i][node]
+                if node < 0: 
+                    return -1
+        return node
+
+    # 返回 x 和 y 的最近公共祖先
+    def get_lca(self, x: int, y: int) -> int:
+        if self.depth[x] > self.depth[y]:
+            x, y = y, x
+        # 使 y 和 x 在同一深度
+        y = self.get_kth_ancestor(y, self.depth[y] - self.depth[x])
+        if y == x:
+            return x
+        pa = self.pa
+        for i in range(len(pa) - 1, -1, -1):
+            px, py = pa[i][x], pa[i][y]
+            if px != py:
+                x, y = px, py  # 同时往上跳 2**i 步
+        return pa[0][x]
+
+    # 返回 x 到 y 的距离（最短路长度）
+    def get_dis(self, x: int, y: int) -> int:
+        return self.depth[x] + self.depth[y] - self.depth[self.get_lca(x, y)] * 2
 
 # 作者：灵茶山艾府
 # 链接：https://leetcode.cn/discuss/post/K0n2gO/
