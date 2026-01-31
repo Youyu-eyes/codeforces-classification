ee3629b8519fbfa543ffa1c87e40a51c373fee64
Youyu-eyes <2630921077@qq.com>
2026-01-31 15:08:27 +0800

    做题模板 cpp

---
+++ 代码模版/做题模板.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+const int MOD = 1'000'000'007;
+
+void solve() {
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int T;
+    cin >> T;
+    
+    while (T--) {
+        solve();
+    }
+    
+    return 0;
+}
