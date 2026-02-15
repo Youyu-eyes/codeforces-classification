d5709da3224a509c190784fd3905989d52c8eeff
Youyu-eyes <2630921077@qq.com>
2026-02-15 16:33:12 +0800

    字典树

--- 代码模版/数据结构/Trie.cpp
+++ 代码模版/数据结构/Trie.cpp
@@ -110,3 +110,87 @@ public:
         return ans;
     }
 };
+
+// 可持久化字典树
+class PersistentTrie {
+public:
+    PersistentTrie(int max_bit = 30) : max_bit(max_bit) {
+        ch0.push_back(0);
+        ch1.push_back(0);
+        cnt.push_back(0);
+        roots.push_back(0);
+    }
+
+    void insert(int prev_root, int value) {
+        int new_root = new_node();
+        int cur_new = new_root;
+        int cur_old = prev_root;
+        cnt[cur_new] = cnt[cur_old] + 1;
+
+        for (int bit = max_bit - 1; bit >= 0; --bit) {
+            int b = (value >> bit) & 1;
+            if (b == 0) {
+                ch1[cur_new] = ch1[cur_old];
+                int left_new = new_node();
+                ch0[cur_new] = left_new;
+                int old_left = ch0[cur_old];
+                cnt[left_new] = cnt[old_left] + 1;
+                cur_new = left_new;
+                cur_old = old_left;
+            } else {
+                ch0[cur_new] = ch0[cur_old];
+                int right_new = new_node();
+                ch1[cur_new] = right_new;
+                int old_right = ch1[cur_old];
+                cnt[right_new] = cnt[old_right] + 1;
+                cur_new = right_new;
+                cur_old = old_right;
+            }
+        }
+        roots.push_back(new_root);
+    }
+
+    int max_xor(int root1, int root2, int value) const {
+        int node1 = root1;
+        int node2 = root2;
+        int res = 0;
+        for (int bit = max_bit - 1; bit >= 0; --bit) {
+            int b = (value >> bit) & 1;
+            if (b == 0) {
+                if (cnt[ch1[node1]] - cnt[ch1[node2]] > 0) {
+                    res |= (1 << bit);
+                    node1 = ch1[node1];
+                    node2 = ch1[node2];
+                } else {
+                    node1 = ch0[node1];
+                    node2 = ch0[node2];
+                }
+            } else {
+                if (cnt[ch0[node1]] - cnt[ch0[node2]] > 0) {
+                    res |= (1 << bit);
+                    node1 = ch0[node1];
+                    node2 = ch0[node2];
+                } else {
+                    node1 = ch1[node1];
+                    node2 = ch1[node2];
+                }
+            }
+        }
+        return res;
+    }
+
+    vector<int> roots;
+
+private:
+    int new_node() {
+        ch0.push_back(0);
+        ch1.push_back(0);
+        cnt.push_back(0);
+        return cnt.size() - 1;
+    }
+
+    int max_bit;
+    vector<int> ch0;
+    vector<int> ch1;
+    vector<int> cnt;
+};
