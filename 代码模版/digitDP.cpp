d99be211a4303cfccb10876bdbafa27cf60e2608
Youyu-eyes <2630921077@qq.com>
2026-03-13 16:32:28 +0800

    数位 DP

--- 代码模版/digitDP.cpp
+++ 代码模版/digitDP.cpp
@@ -56,3 +56,59 @@ long long digitDP(long long low, long long high, int target) {
 
     return dfs(0, 0, true, true);
 }
+
+
+// 满足条件数字的价值总和
+// 计算在 [low, high] 中的整数 x 的数位和，满足 x 中的不同数字个数不超过 k
+long long digitDPContribution(long long low, long long high, int k) {
+    string low_s = to_string(low);
+    string high_s = to_string(high);
+    int n = high_s.size();
+    int diff_lh = n - low_s.size();
+    vector memo(n, vector<pair<long long, long long>>(1 << 10, {-1, -1}));
+
+    // dfs 返回两个数：子树合法数字个数，子树数位总和
+    auto dfs = [&](this auto&& dfs, int i, int mask, bool limit_low, bool limit_high) -> pair<long long, long long> {
+        if (i == n) {
+            // 如果没有特殊约束，那么能递归到终点的都是合法数字
+            return {1, 0};
+        }
+
+        if (!limit_low && !limit_high && memo[i][mask].first >= 0) {
+            return memo[i][mask];
+        }
+
+        int lo = limit_low && i >= diff_lh ? low_s[i - diff_lh] - '0' : 0;
+        int hi = limit_high ? high_s[i] - '0' : 9;
+
+        long long cnt = 0, sum = 0;
+        int d = lo;
+
+        // 如果前导零不影响答案，去掉这个 if block
+        if (limit_low && i < diff_lh) {
+            // 不填数字，上界不受约束
+            tie(cnt, sum) = dfs(i + 1, 0, true, false);
+            d = 1;
+        }
+
+        for (; d <= hi; d++) {
+            int new_mask = mask | 1 << d;
+            if (popcount((uint32_t) new_mask) > k) { // 不满足要求
+                continue;
+            }
+            auto [sub_cnt, sub_sum] = dfs(i + 1, new_mask, limit_low && d == lo, limit_high && d == hi);
+            cnt += sub_cnt; // 累加子树的合法数字个数
+            sum += sub_sum; // 累加子树的数位总和
+            sum += d * sub_cnt; // d 会出现在 sub_cnt 个数中（贡献法）
+            // cnt %= MOD; sum %= MOD;
+        }
+
+        pair<long long, long long> res = {cnt, sum};
+        if (!limit_low && !limit_high) {
+            memo[i][mask] = res;
+        }
+        return res;
+    };
+
+    return dfs(0, 0, true, true).second;
+}
--- 代码模版/digitDP.py
+++ 代码模版/digitDP.py
@@ -1,4 +1,6 @@
 from functools import cache
+from collections import deque
+from typing import List, Tuple
 
 def digitDP(low: int, high: int, target: int) -> int:
     low_s = list(map(int, str(low)))
@@ -41,3 +43,47 @@ def digitDP(low: int, high: int, target: int) -> int:
         return res
 
     return dfs(0, 0, True, True)
+
+# 满足条件数字的价值总和
+# 计算在 [low, high] 中的整数 x 的数位和，满足 x 中的不同数字个数不超过 k
+def digitDPContribution(low: int, high: int, k: int) -> int:
+    low_s = list(map(int, str(low)))  # 避免在 dfs 中频繁调用 int()
+    high_s = list(map(int, str(high)))
+    n = len(high_s)
+    diff_lh = n - len(low_s)
+
+    # dfs 返回两个数：子树合法数字个数，子树数位总和
+    @cache
+    def dfs(i: int, mask: int, limit_low: bool, limit_high: bool) -> Tuple[int, int]:
+        if i == n:
+            # 如果没有特殊约束，那么能递归到终点的都是合法数字
+            return 1, 0
+
+        lo = low_s[i - diff_lh] if limit_low and i >= diff_lh else 0
+        hi = high_s[i] if limit_high else 9
+
+        cnt = res = 0
+        start = lo
+
+        # 如果前导零不影响答案，去掉这个 if block
+        if limit_low and i < diff_lh:
+            # 不填数字，上界不受约束
+            cnt, res = dfs(i + 1, 0, True, False)
+            start = 1
+
+        for d in range(start, hi + 1):
+            new_mask = mask | 1 << d
+            if new_mask.bit_count() > k:  # 不满足要求
+                continue
+            sub_cnt, sub_sum = dfs(i + 1,
+                                   new_mask,
+                                   limit_low and d == lo,
+                                   limit_high and d == hi)
+            cnt += sub_cnt  # 累加子树的合法数字个数
+            res += sub_sum  # 累加子树的数位总和
+            res += d * sub_cnt  # d 会出现在 sub_cnt 个数中（贡献法）
+
+        # cnt %= MOD; res %= MOD
+        return cnt, res
+
+    return dfs(0, 0, True, True)[1]
