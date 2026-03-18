fe5aa5840106abf61cf20f20634f4980a924328d
Youyu-eyes <2630921077@qq.com>
2026-03-18 20:48:58 +0800

    单调栈

--- 2025年ZUFE校赛/B_阿莫古斯与安娜尔的观景问题.cpp
+++ 2025年ZUFE校赛/B_阿莫古斯与安娜尔的观景问题.cpp
@@ -1,3 +1,17 @@
+// https://cpc.zufe.ac.cn/contest/7/sub/5/problem/B
+// 单调栈
+// min(nums[l], nums[r]) < all(nums[l + 1, r - 1])
+// 形象的说，像一个碗，如果两端比中间都大，就能盛水
+// 分类讨论
+// 假设 min(nums[l], nums[r]) = nums[r]
+// 对于每一个 i，找到 i 左端第一个大于等于 nums[i] 的数的下标 j，则在 nums[j, i] 之间的数都满足小于 nums[i] 的条件
+// 而对于 j 左端的数，假设下标 k，在 nums[k, i] 的区间内，有 nums[j] > nums[i] 不符合题意
+// 因此对于 i，j 是 i 左端点的最大答案
+// 同理，假设 min(nums[l], nums[r]) = nums[l]
+// 则找右端点第一个大于等于 nums[i] 的下标
+
+// 代码实现时可以把正向的单调栈封装成函数，然后将 nums 反转后再调用
+
 #include <bits/stdc++.h>
 using namespace std;
 
