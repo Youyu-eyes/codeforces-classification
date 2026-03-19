680f391124b216536cf87eff5d907d385978025b
Youyu-eyes <2630921077@qq.com>
2026-03-19 10:54:42 +0800

    单调栈

--- 代码模版/数据结构/monotonic stack.py
+++ 代码模版/数据结构/monotonic stack.py
@@ -1,6 +1,7 @@
 from collections import deque
 from typing import List, Tuple
 
+# 找到 i 左端第一个严格大于 nums[i] 的数的下标
 def nearestGreater(nums: List[int]) -> Tuple[List[int], List[int]]:
     n = len(nums)
     # left[i] 是 nums[i] 左侧最近的严格大于 nums[i] 的数的下标，若不存在则为 -1
@@ -25,3 +26,17 @@ def nearestGreater(nums: List[int]) -> Tuple[List[int], List[int]]:
         st.append(i)
 
     return left, right
+
+# 找到离 i 最远的，比 nums[i] 小的数
+def longestWPI(self, hours: List[int]) -> int:
+    n = len(hours)
+    s = [0] * (n + 1)  # 前缀和
+    st = [0]  # s[0]
+    for j, h in enumerate(hours, 1):
+        s[j] = s[j - 1] + (1 if h > 8 else -1)
+        if s[j] < s[st[-1]]: st.append(j)  # 感兴趣的 j
+    ans = 0
+    for i in range(n, 0, -1):
+        while st and s[i] > s[st[-1]]:
+            ans = max(ans, i - st.pop())  # [st[-1],i) 可能是最长子数组
+    return ans
