f5434615cb2ca120d8cf8598b76b1938499811d5
Youyu-eyes <2630921077@qq.com>
2026-03-19 10:56:38 +0800

    单调栈

--- 代码模版/数据结构/monotonic stack.cpp
+++ 代码模版/数据结构/monotonic stack.cpp
@@ -34,3 +34,20 @@ pair<vector<int>, vector<int>> nearestGreater(vector<int>& nums) {
 
     return {left, right};
 }
+
+// 在 i 左侧离 i 最远的 下标 j
+int longestWPI(vector<int> &hours) {
+    int n = hours.size(), ans = 0, s[n + 1]; // 前缀和
+    stack<int> st;
+    st.push(s[0] = 0);
+    for (int j = 1; j <= n; ++j) {
+        s[j] = s[j - 1] + (hours[j - 1] > 8 ? 1 : -1);
+        if (s[j] < s[st.top()]) st.push(j); // 感兴趣的 j
+    }
+    for (int i = n; i; --i)
+        while (!st.empty() && s[i] > s[st.top()]) {
+            ans = max(ans, i - st.top()); // [栈顶,i) 可能是最长子数组
+            st.pop();
+        }
+    return ans;
+}
