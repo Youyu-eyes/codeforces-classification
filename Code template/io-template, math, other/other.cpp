3c5e92dd661b1d5e372eea567413b4397388d938
Youyu-eyes <2630921077@qq.com>
2026-04-03 14:43:50 +0800

    cpp23 学习笔记

--- Code template/io-template, math, other/other.cpp
+++ Code template/io-template, math, other/other.cpp
@@ -6,6 +6,8 @@ using ll = long long;
 int main() {
     int n;
     vector nums(n, vector<int>(n));
+    vector<int> nums1(n);
+    vector<int> nums2(n);
 
     // 以数组第一项升序
     ranges::sort(nums, {}, [](auto& a) { return a[0]; });
@@ -15,6 +17,10 @@ int main() {
     ranges::sort(nums, [](const auto& a, const auto& b) {
         return tie(a[0], b[1]) < tie(b[0], a[1]);
     });
+    // nums1 与 nums2 下标一一对应，实现 nums1 升序排序，nums2 保持下标对应排序
+    auto zipped = views::zip(nums1, nums2);
+    ranges::sort(zipped, std::less{},
+                  [](const auto& t) { return std::get<0>(t); });
 
     return 0;
 }
