18f8de661bc865983d4b71e7a8e082d655a65c02
Youyu-eyes <2630921077@qq.com>
2026-04-09 23:42:57 +0800

    凸包优化DP模板

--- Code template/DP/Convex Hull Trick.cpp
+++ Code template/DP/Convex Hull Trick.cpp
@@ -6,6 +6,9 @@ const int MOD = 1'000'000'007;
 const int inf = 0x3f3f3f3f;
 const ll ll_inf = 1e18;
 
+// 在使用模板时要注意：
+// 在维护凸包时要保证查询向量 p 的 y 值 p.y > 0，这样我们才能用 max or min 简单地判断用 上凸包 or 下凸包
+
 struct Vec {
     ll x, y;
     Vec(ll x = 0, ll y = 0) : x(x), y(y) {}
@@ -15,8 +18,8 @@ struct Vec {
     ll dot(const Vec& other) const {
         return x * other.x + y * other.y;
     }
-    ll det(const Vec& other) const {
-        return x * other.y - y * other.x;
+    __int128 det(const Vec& other) const {
+        return (__int128) x * other.y - (__int128) y * other.x;
     }
 };
 
@@ -24,23 +27,21 @@ struct Vec {
 struct UpperHull {
     deque<Vec> hull;
     void add(const Vec& p) {
-        while (hull.size() > 1) {
-            Vec& back1 = hull.back();
-            Vec& back2 = hull[hull.size()-2];
-            // 维护上凸包：新点使前一个点成为凹点则弹出
-            if ((p - back1).det(back1 - back2) >= 0) hull.pop_back();
-            else break;
+        while (hull.size() > 1 && (p - hull.back()).det(hull.back() - hull[hull.size() - 2]) <= 0) {
+            hull.pop_back();
         }
+
         hull.push_back(p);
     }
-    // 单调查询（假设查询向量 p 的斜率单调递增，即 p.x 递增？）
-    // 这里假设 p 的 x 单调递增，对于上凸包，最大值点单调右移
+
+    // 单调查询，这里假设 p 的 x 单调递增，最大值点单调右移
     long long query_monotonic(const Vec& p) {
         while (hull.size() > 1 && p.dot(hull[0]) <= p.dot(hull[1]))
             hull.pop_front();
         return p.dot(hull.front());
     }
-    // 二分查询（p 不单调）
+
+    // 二分查询最大值
     long long query_binary(const Vec& p) const {
         int l = 0, r = hull.size()-1;
         while (l < r) {
@@ -58,21 +59,20 @@ struct UpperHull {
 struct LowerHull {
     deque<Vec> hull;
     void add(const Vec& p) {
-        while (hull.size() > 1) {
-            Vec& back1 = hull.back();
-            Vec& back2 = hull[hull.size()-2];
-            // 维护下凸包：新点使前一个点成为凸点？条件相反
-            if ((p - back1).det(back1 - back2) <= 0) hull.pop_back();
-            else break;
+        while (hull.size() > 1 && (p - hull.back()).det(hull.back() - hull[hull.size() - 2]) >= 0) {
+            hull.pop_back();
         }
+
         hull.push_back(p);
     }
-    // 单调查询（假设 p 的 x 单调递增，最小值点单调右移）
+
+    // 单调查询，这里假设 p 的 x 单调递增，最小值点单调右移
     long long query_monotonic(const Vec& p) {
         while (hull.size() > 1 && p.dot(hull[0]) >= p.dot(hull[1]))
             hull.pop_front();
         return p.dot(hull.front());
     }
+
     // 二分查询最小值
     long long query_binary(const Vec& p) const {
         int l = 0, r = hull.size()-1;
