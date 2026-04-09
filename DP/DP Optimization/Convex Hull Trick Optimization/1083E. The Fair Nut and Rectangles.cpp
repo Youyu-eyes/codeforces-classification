eaf692b74dfaf9f5aa0315415bf20818c573c4e8
Youyu-eyes <2630921077@qq.com>
2026-04-10 00:32:50 +0800

    子序列DP + 凸包优化

--- DP/DP Optimization/Convex Hull Trick Optimization/1083E. The Fair Nut and Rectangles.cpp
+++ DP/DP Optimization/Convex Hull Trick Optimization/1083E. The Fair Nut and Rectangles.cpp
@@ -31,21 +31,27 @@ const ll ll_inf = 1e18;
 struct Vec {
     ll x, y;
     Vec(ll x = 0, ll y = 0) : x(x), y(y) {}
+
     Vec operator-(const Vec& other) const {
         return Vec(x - other.x, y - other.y);
     }
+
     ll dot(const Vec& other) const {
         return x * other.x + y * other.y;
     }
+
+    // a.det(b) > 0 => a 到 b 逆时针
+    // a.det(b) < 0 => a 到 b 顺时针
     __int128 det(const Vec& other) const {
         return (__int128) x * other.y - (__int128) y * other.x;
     }
 };
 
+// 上凸包（求最大值）
 struct UpperHull {
     deque<Vec> hull;
     void add(const Vec& p) {
-        while (hull.size() > 1 && (p - hull.back()).det(hull.back() - hull[hull.size() - 2]) <= 0) {
+        while (hull.size() > 1 && (hull.back() - hull[hull.size() - 2]).det(p - hull.back()) >= 0) {
             hull.pop_back();
         }
 
@@ -53,13 +59,14 @@ struct UpperHull {
     }
 
     // 单调查询，这里假设 p 的 x 单调递增，最大值点单调右移
+    // 复杂度 O(n)
     long long query_monotonic(const Vec& p) {
         while (hull.size() > 1 && p.dot(hull[0]) <= p.dot(hull[1]))
             hull.pop_front();
         return p.dot(hull.front());
     }
 
-    // 二分查询最大值
+    // 二分查询最大值，复杂度 O(nlogn)
     long long query_binary(const Vec& p) const {
         int l = 0, r = hull.size()-1;
         while (l < r) {
