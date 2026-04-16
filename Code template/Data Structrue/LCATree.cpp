bbfbfccf6c25c58edbd82c2c70b41f29b85face3
Youyu-eyes <2630921077@qq.com>
2026-04-16 14:16:28 +0800

    LCA 最近公共祖先

--- Code template/Data Structrue/LCATree.cpp
+++ Code template/Data Structrue/LCATree.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 带权 LCA 模板
 class LcaBinaryLifting {
     vector<int> depth;
     vector<long long> dis; // 如果是无权树（边权为 1），dis 可以去掉，用 depth 代替
@@ -74,5 +75,82 @@ public:
     // 返回 x 到 y 的距离（最短路长度）
     long long get_dis(int x, int y) {
         return dis[x] + dis[y] - dis[get_lca(x, y)] * 2;
+    }
+};
+
+
+// 不带权 LCA 模板
+class LcaBinaryLifting {
+    std::vector<int> depth;
+    std::vector<std::vector<int>> pa;
+
+public:
+    // edges: 每条边为 [x, y]，表示树中的一条边，节点编号从 0 开始
+    LcaBinaryLifting(std::vector<std::vector<int>>& edges) {
+        int n = (int)edges.size() + 1;
+        int m = std::bit_width((unsigned)n); // n 的二进制长度
+
+        std::vector<std::vector<int>> g(n);
+        for (auto& e : edges) {
+            int x = e[0], y = e[1];
+            g[x].push_back(y);
+            g[y].push_back(x);
+        }
+
+        depth.resize(n);
+        pa.resize(m, std::vector<int>(n, -1));
+
+        auto dfs = [&](this auto&& dfs, int x, int fa) -> void {
+            pa[0][x] = fa;
+            for (int y : g[x]) {
+                if (y != fa) {
+                    depth[y] = depth[x] + 1;
+                    dfs(y, x);
+                }
+            }
+        };
+        dfs(0, -1);
+
+        for (int i = 0; i < m - 1; ++i) {
+            for (int x = 0; x < n; ++x) {
+                if (int p = pa[i][x]; p != -1) {
+                    pa[i + 1][x] = pa[i][p];
+                }
+            }
+        }
+    }
+
+    // 返回 node 的第 k 个祖先节点
+    // 如果不存在，返回 -1
+    int get_kth_ancestor(int node, int k) {
+        for (; k > 0 && node >= 0; k &= k - 1) {
+            node = pa[std::countr_zero((unsigned)k)][node];
+        }
+        return node;
+    }
+
+    // 返回 x 和 y 的最近公共祖先（节点编号从 0 开始）
+    int get_lca(int x, int y) {
+        if (depth[x] > depth[y]) {
+            std::swap(x, y);
+        }
+        y = get_kth_ancestor(y, depth[y] - depth[x]); // 使 y 和 x 在同一深度
+        if (y == x) {
+            return x;
+        }
+        for (int i = (int)pa.size() - 1; i >= 0; --i) {
+            int px = pa[i][x], py = pa[i][y];
+            if (px != py) {
+                x = px;
+                y = py; // 同时往上跳 2^i 步
+            }
+        }
+        return pa[0][x];
+    }
+
+    // 返回 x 到 y 的距离（最短路长度，边权为 1）
+    int get_dis(int x, int y) {
+        int l = get_lca(x, y);
+        return depth[x] + depth[y] - 2 * depth[l];
     }
 };
