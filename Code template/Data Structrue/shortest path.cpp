ec9b0307ec23fc37288bb92f5afc57f8610721d6
Youyu-eyes <2630921077@qq.com>
2026-04-16 19:38:46 +0800

    最短路问题

--- Code template/Data Structrue/shortest path.cpp
+++ Code template/Data Structrue/shortest path.cpp
@@ -3,12 +3,40 @@ using namespace std;
 
 using ll = long long;
 const int MOD = 1'000'000'007;
-const int inf = INT_MAX;
-const ll ll_inf = LLONG_MAX;
+const int inf = 0x3f3f3f3f;
+const ll ll_inf = 1e18;
+
+
+// 0-1 BFS
+// 计算从 start 到各个节点的最短路长度
+// 如果节点不可达，则最短路长度为 inf
+// 节点编号从 0 到 n-1，边权为 0 or 1
+vector<int> zero_one_bfs(int n, vector<vector<pair<int, int>>>& g, int start) {
+    vector<int> dis(n, inf);
+    deque<int> q;
+    q.push_back(start);
+
+    while (!q.empty()) {
+        int x = q.front();
+        q.pop_front();
+
+        for (auto &[y, w] : g[x]) {
+            if (dis[x] + w < dis[y]) {
+                dis[y] = dis[x] + w;
+                if (w == 0) {
+                    q.push_front(y);
+                } else {
+                    q.push_back(y);
+                }
+            }
+        }
+    }
+    return dis;
+}
 
 
 // dijkstra：单源最短路
-// 返回从起点 start 到每个点的最短路长度 dis，如果节点 x 不可达，则 dis[x] = LLONG_MAX
+// 返回从起点 start 到每个点的最短路长度 dis，如果节点 x 不可达，则 dis[x] = ll_inf
 // 要求：没有负数边权
 // 时间复杂度 O(n + mlogm)，注意堆中有 O(m) 个元素
 vector<long long> shortestPathDijkstra(int n, vector<vector<int>>& edges, int start) {
@@ -20,7 +48,7 @@ vector<long long> shortestPathDijkstra(int n, vector<vector<int>>& edges, int st
         // g[y].emplace_back(x, wt); // 无向图加上这行
     }
 
-    vector<long long> dis(n, LLONG_MAX);
+    vector<long long> dis(n, ll_inf);
     // 堆中保存 (起点到节点 x 的最短路长度，节点 x)
     priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
     dis[start] = 0; // 起点到自己的距离是 0
@@ -50,13 +78,13 @@ vector<long long> shortestPathDijkstra(int n, vector<vector<int>>& edges, int st
 // Floyd：全源最短路
 
 // 返回一个二维列表，其中 (i,j) 这一项表示从 i 到 j 的最短路长度
-// 如果无法从 i 到 j，则最短路长度为 LLONG_MAX / 2
+// 如果无法从 i 到 j，则最短路长度为 ll_inf / 2
 // 允许负数边权
 // 如果计算完毕后，存在 i，使得从 i 到 i 的最短路长度小于 0，说明图中有负环
 // 节点编号从 0 到 n-1
 // 时间复杂度 O(n^3 + m)，其中 m 是 edges 的长度
 vector<vector<long long>> shortestPathFloyd(int n, vector<vector<int>>& edges) {
-    const long long INF = LLONG_MAX / 2; // 防止加法溢出
+    const long long INF = ll_inf / 2; // 防止加法溢出
     vector f(n, vector<long long>(n, INF));
     for (int i = 0; i < n; i++) {
         f[i][i] = 0;
--- Code template/Data Structrue/shortest path.py
+++ Code template/Data Structrue/shortest path.py
@@ -1,8 +1,32 @@
 from heapq import heappop, heappush
+from collections import deque
 from math import inf
 from typing import List, Tuple
 
+# 0-1 BFS
+# 计算从 start 到各个节点的最短路长度
+# 如果节点无法到达，最短路为 inf
+# 节点编号从 0 到 n-1，边权为 0 or 1
+
+def zero_one_bfs(n: int, g: List[List[Tuple[int, int]]], start: int) -> List[int]:
+    dis = [inf] * n
+    dis[start] = 0
+    q = deque([start])
+
+    while q:
+        x = q.popleft()
+        for y, w in g[x]:
+            if dis[x] + w < dis[y]:
+                dis[y] = dis[x] + w
+                if w == 0:
+                    q.appendleft(y)
+                else:
+                    q.append(y)
+    return dis
+
+
 # dijkstra：单源最短路
+
 def shortestPathDijkstra(n: int, edges: List[List[int]], start: int) -> List[int]:
     # 注：如果节点编号从 1 开始（而不是从 0 开始），可以把 n 加一
     g = [[] for _ in range(n)]  # 邻接表
