665681abe2d0f4d800c8b5552728a0c702d48979
Youyu-eyes <2630921077@qq.com>
2026-04-16 19:30:14 +0800

    0 - 1 BFS

--- Code template/Data Structrue/0-1BFS.cpp
+++ Code template/Data Structrue/0-1BFS.cpp
@@ -3,31 +3,29 @@ using namespace std;
 
 using ll = long long;
 const int MOD = 1'000'000'007;
-const int inf = INT_MAX;
-const ll ll_inf = LLONG_MAX;
+const int inf = 0x3f3f3f3f;
+const ll ll_inf = 1e18;
 
 // 计算从 start 到各个节点的最短路长度
-// 如果节点不可达，则最短路长度为 -1
-// 节点编号从 0 到 n-1，边权均为 1
-vector<int> bfs(int n, vector<vector<int>>& edges, int start) {
-    vector<vector<int>> g(n);
-    for (auto& e : edges) {
-        int x = e[0], y = e[1];
-        g[x].push_back(y);
-        g[y].push_back(x); // 无向图
-    }
+// 如果节点不可达，则最短路长度为 inf
+// 节点编号从 0 到 n-1，边权为 0 or 1
+vector<int> zero_one_bfs(int n, vector<vector<pair<int, int>>>& g, int start) {
+    vector<int> dis(n, inf);
+    deque<int> q;
+    q.push_back(start);
 
-    vector<int> dis(n, -1); // -1 表示尚未访问到
-    queue<int> q;
-    dis[start] = 0;
-    q.push(start);
     while (!q.empty()) {
         int x = q.front();
-        q.pop();
-        for (int y : g[x]) {
-            if (dis[y] < 0) {
-                dis[y] = dis[x] + 1;
-                q.push(y);
+        q.pop_front();
+
+        for (auto &[y, w] : g[x]) {
+            if (dis[x] + w < dis[y]) {
+                dis[y] = dis[x] + w;
+                if (w == 0) {
+                    q.push_front(y);
+                } else {
+                    q.push_back(y);
+                }
             }
         }
     }
--- Code template/Data Structrue/0-1BFS.py
+++ Code template/Data Structrue/0-1BFS.py
@@ -1,21 +1,22 @@
 from collections import deque
 from typing import List, Tuple
+from math import inf
 # 计算从 start 到各个节点的最短路长度
-# 如果节点不可达，则最短路长度为 -1
-# 节点编号从 0 到 n-1，边权均为 1
-def bfs(n: int, edges: List[List[int]], start: int) -> List[int]:
-    g = [[] for _ in range(n)]
-    for x, y in edges:
-        g[x].append(y)
-        g[y].append(x)  # 无向图
+# 如果节点无法到达，最短路为 inf
+# 节点编号从 0 到 n-1，边权为 0 or 1
 
-    dis = [-1] * n  # -1 表示尚未访问到
+def zero_one_bfs(n: int, g: List[List[Tuple[int, int]]], start: int) -> List[int]:
+    dis = [inf] * n
     dis[start] = 0
     q = deque([start])
+
     while q:
         x = q.popleft()
-        for y in g[x]:
-            if dis[y] < 0:
-                dis[y] = dis[x] + 1
-                q.append(y)
+        for y, w in g[x]:
+            if dis[x] + w < dis[y]:
+                dis[y] = dis[x] + w
+                if w == 0:
+                    q.appendleft(y)
+                else:
+                    q.append(y)
     return dis
