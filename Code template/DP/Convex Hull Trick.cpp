fe29d4b1653c10bbddc8b2c83a7af6f1c1d090e9
Youyu-eyes <2630921077@qq.com>
2026-04-22 22:38:37 +0800

    凸包模板

--- Code template/DP/Convex Hull Trick.cpp
+++ Code template/DP/Convex Hull Trick.cpp
@@ -45,17 +45,14 @@ struct UpperHull {
     // 如果 v0.x 单调递减，则 UpperHull.query_monotonic(v0, -1)
     // 复杂度 O(n)
     long long query_monotonic(const Vec& p, int dir) {
-        long long n = hull.size();
         if (dir > 0) {
-            while (n > 1 && p.dot(hull[0]) <= p.dot(hull[1])) {
+            while (hull.size() > 1 && p.dot(hull[0]) <= p.dot(hull[1])) {
                 hull.pop_front();
             }
             return p.dot(hull.front());
-        }
-        
-        else {
-            while (n > 1 && p.dot(hull.back()) <= p.dot(hull[n - 2])) {
-                hull.pop_front();
+        } else {
+            while (hull.size() > 1 && p.dot(hull.back()) <= p.dot(hull[hull.size() - 2])) {
+                hull.pop_back();
             }
             return p.dot(hull.back());
         }
