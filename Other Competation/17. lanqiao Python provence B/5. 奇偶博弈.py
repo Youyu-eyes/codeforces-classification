0d80a650ba6b7cb785534b8c53012511e019bc1d
Youyu-eyes <2630921077@qq.com>
2026-04-23 19:12:42 +0800

    17 届蓝桥 py 省B

--- Other Competation/17. lanqiao Python provence B/5. 奇偶博弈.py
+++ Other Competation/17. lanqiao Python provence B/5. 奇偶博弈.py
@@ -40,39 +40,6 @@
 
 # MOD = 1_000_000_007
 
-class UnionFind:
-    def __init__(self, n: int):
-        self._fa = list(range(n))
-        self._size = [1] * n
-        self.cc = n
-        self.lazy = [0] * n
-        self.d = [0] * n
-
-    def find(self, x: int) -> int:
-        fa = self._fa
-        if fa[x] != x:
-            root = self.find(fa[x])
-            self.d[x] += self.d[fa[x]]
-            fa[x] = root
-        return fa[x]
-
-    def is_same(self, x: int, y: int) -> bool:
-        return self.find(x) == self.find(y)
-
-    def merge(self, from_: int, to: int) -> bool:
-        x, y = self.find(from_), self.find(to)
-        if x == y:
-            return False
-
-        self.d[x] = self.lazy[x] - self.lazy[y]
-        self._fa[x] = y
-        self._size[y] += self._size[x]
-        self.cc -= 1
-        return True
-
-    def get_size(self, x: int) -> int:
-        return self._size[self.find(x)]
-
 import sys
 input = lambda: sys.stdin.readline().rstrip('\r\n')
 print = lambda *args, end='\n', sep=' ': sys.stdout.write(
@@ -88,24 +55,16 @@ def MII(b = 0):
 def LII(b = 0):
     return list(MII(b))
 
-
 for _ in range(II()):
-    n, Q = MII()
-    val = [0] * (n + 1)
-    uf = UnionFind(n + 1)
-    for q in range(Q):
-        cur = LII()
-        if cur[0] == 1:
-            x, y = cur[1], cur[2]
-            uf.merge(x, y)
-        elif cur[0] == 2:
-            x, a = cur[1], cur[2]
-            val[x] += a
-        elif cur[0] == 3:
-            x, a = cur[1], cur[2]
-            root = uf.find(x)
-            uf.lazy[root] += a
+    n = II()
+    ans = 0
+    nums = MII()
+    for x in nums:
+        if x == 1:
+            ans += 1
         else:
-            root = uf.find(x)
-            ans = val[x] + uf.d[x] + uf.lazy[root]
-            print(ans)
+            ans += 2
+    if ans & 1:
+        print('L')
+    else:
+        print('Q')
