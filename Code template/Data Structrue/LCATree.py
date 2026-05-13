e322b77eca7490d874493ec7c3b6c26a9c0ceb07
Youyu-eyes <2630921077@qq.com>
2026-05-13 19:01:49 +0800

    LCA最近公共祖先

--- Code template/Data Structrue/LCATree.py
+++ Code template/Data Structrue/LCATree.py
@@ -122,8 +122,3 @@ class LcaBinaryLifting:
     # 返回 x 到 y 的距离（最短路长度）
     def get_dis(self, x: int, y: int) -> int:
         return self.depth[x] + self.depth[y] - self.depth[self.get_lca(x, y)] * 2
-
-# 作者：灵茶山艾府
-# 链接：https://leetcode.cn/discuss/post/K0n2gO/
-# 来源：力扣（LeetCode）
-# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
